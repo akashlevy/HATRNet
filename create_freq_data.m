effbe7e562a17d24d0c93a0de0354ad3d3a536c8
messnerj <messnerj@stanford.edu>
2019-02-25 18:15:48 -0800

    used raw data as input

--- create_freq_data.m
+++ create_freq_data.m
@@ -1,18 +1,22 @@
 clear all;
 close all;
 
-load('data/data1.mat'); data=data1;
 Fs = 50;
-T = 1/Fs;
 
-data = data - mean(data,2);
-data = data ./ std(data,0,2);
+labels = importdata('data/HAPT_dataset/RawData/labels.txt');
+max_length = max(labels(:,5)-labels(:,4))+1;
 
-for i=1:size(data,1)
-    [~,data_fft(i,:,1)] = fft_freq(data(i,:,1), Fs, 2000);
-    [~,data_fft(i,:,2)] = fft_freq(data(i,:,2), Fs, 2000);
-    [~,data_fft(i,:,3)] = fft_freq(data(i,:,3), Fs, 2000);
-    [~,data_fft(i,:,4)] = fft_freq(data(i,:,4), Fs, 2000);
-    [~,data_fft(i,:,5)] = fft_freq(data(i,:,5), Fs, 2000);
-    [~,data_fft(i,:,6)] = fft_freq(data(i,:,6), Fs, 2000);
+for i=1:length(labels)
+    acc = importdata(['data/HAPT_dataset/RawData/acc_exp',num2str(labels(i,1),'%02.f'),'_user',num2str(labels(i,2),'%02.f'),'.txt']);
+    gyro = importdata(['data/HAPT_dataset/RawData/gyro_exp',num2str(labels(i,1),'%02.f'),'_user',num2str(labels(i,2),'%02.f'),'.txt']);
+    
+    data_start = labels(i,4);
+    data_end = labels(i,5);
+    
+    data_fft(i,:,1) = fft_freq(acc(data_start:data_end,1), Fs, 2000);
+    data_fft(i,:,2) = fft_freq(acc(data_start:data_end,2), Fs, 2000);
+    data_fft(i,:,3) = fft_freq(acc(data_start:data_end,3), Fs, 2000);
+    data_fft(i,:,4) = fft_freq(gyro(data_start:data_end,1), Fs, 2000);
+    data_fft(i,:,5) = fft_freq(gyro(data_start:data_end,2), Fs, 2000);
+    data_fft(i,:,6) = fft_freq(gyro(data_start:data_end,3), Fs, 2000);    
 end
