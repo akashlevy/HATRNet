5b740c6bcfa7829705f83bf9c920ad4345643dd2
Jonas Messner <messnerj@stanford.edu>
2019-03-12 20:48:43 -0700

    updated

--- Hyperparameter_Search/hyperparam_eval.m
+++ Hyperparameter_Search/hyperparam_eval.m
@@ -22,7 +22,9 @@ N = 80;
 filter_coeff = ones(1,N)/N;
 avg_accuracy = filtfilt(filter_coeff, 1, [avg_accuracy_validation(i(1:N));avg_accuracy_validation(i)]);
 figure();
-semilogx(lr_sort,[avg_accuracy_validation(i) avg_accuracy(N+1:end)])
+semilogx(lr_sort,avg_accuracy_validation(i));
+hold on;
+semilogx(lr_sort,avg_accuracy(N+1:end), 'LineWidth', 1.5);
 xlabel('Learning rate');
 ylabel('Validation accuracy in %');
 legend('Raw data', 'Smoothed data');
@@ -37,37 +39,72 @@ avg_loss_validation = avg_loss_validation(i(s:e));
 avg_accuracy_validation = avg_accuracy_validation(i(s:e));
 
 % Base filter number
-[bfn_sort,i] = sort(base_filter_num);
+i=1; stop=max(base_filter_num);
+while i<=stop
+    k = find(base_filter_num==i);
+    if ~isempty(k)
+        bfn_median(i) = median(avg_accuracy_validation(k)*100);
+    end
+    i=i+1;
+end
 figure();
-boxplot(avg_accuracy_validation(i)*100,bfn_sort)
+stem(1:stop,bfn_median);
 xlabel('Number of filters in first 1D convolution block');
-ylabel('Validation accuracy in %');
+ylabel('Median validation accuracy in %');
 
 % Number of 1D Conv blocks
-[c1block_sort,i] = sort(conv1_block);
+i=1; stop=max(conv1_block);
+while i<=stop
+    k = find(conv1_block==i);
+    if ~isempty(k)
+        c1block_median(i) = median(avg_accuracy_validation(k)*100);
+    end
+    i=i+1;
+end
 figure();
-boxplot(avg_accuracy_validation(i)*100,c1block_sort)
+stem(1:stop,c1block_median);
 xlabel('Number of 1D convolution blocks');
 ylabel('Validation accuracy in %');
 
 % Size of 1D Kernel
-[c1kernel_sort,i] = sort(conv1_kernel);
+i=1; stop=max(conv1_kernel);
+while i<=stop
+    k = find(conv1_kernel==i);
+    if ~isempty(k)
+        c1kernel_median(i) = median(avg_accuracy_validation(k)*100);
+    end
+    i=i+1;
+end
 figure();
-boxplot(avg_accuracy_validation(i)*100,c1kernel_sort)
+stem(1:stop,c1kernel_median);
 xlabel('1D convolution kernel size nxn');
 ylabel('Validation accuracy in %');
 
 % Size of 2D Kernel
-[c2kernel_sort,i] = sort(conv2_kernel);
+i=1; stop=max(conv2_kernel);
+while i<=stop
+    k = find(conv2_kernel==i);
+    if ~isempty(k)
+        c2kernel_median(i) = median(avg_accuracy_validation(k)*100);
+    end
+    i=i+1;
+end
 figure();
-boxplot(avg_accuracy_validation(i)*100,c2kernel_sort)
+stem(1:stop,c2kernel_median);
 xlabel('2D convolution kernel size nxn');
 ylabel('Validation accuracy in %');
 
 % Size of dense layer
-[dense_sort,i] = sort(dense_size);
+i=1; stop=max(dense_size);
+while i<=stop
+    k = find(dense_size==i);
+    if ~isempty(k)
+        dsize_median(i) = median(avg_accuracy_validation(k)*100);
+    end
+    i=i+1;
+end
 figure();
-boxplot(avg_accuracy_validation(i)*100,dense_sort)
+stem(1:stop,dsize_median);
 xlabel('Number of neurons in fully-connected layer');
 ylabel('Validation accuracy in %');
 
@@ -78,7 +115,9 @@ N = 80;
 filter_coeff = ones(1,N)/N;
 avg_accuracy = filtfilt(filter_coeff, 1, [avg_accuracy_validation(i(1:N));avg_accuracy_validation(i)]);
 figure();
-plot(drop_sort,[avg_accuracy_validation(i) avg_accuracy(N+1:end)])
+plot(drop_sort,avg_accuracy_validation(i));
+hold on;
+semilogx(drop_sort,avg_accuracy(N+1:end), 'LineWidth', 1.5);
 xlabel('Dropout percentage');
 ylabel('Validation accuracy in %');
 legend('Raw data', 'Smoothed data');
